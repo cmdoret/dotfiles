0d36e516fcd6c821348ae21b048caf4a26b89059
cmdoret <cyril.mattheydoret@gmail.com>
2025-03-12 01:44:40 +0100

    feat(nvchad): include hidden files in telescope find_files

--- .config/nvchad/custom/mappings.lua
+++ .config/nvchad/custom/mappings.lua
@@ -6,4 +6,11 @@ M.maximizer = {
   },
 }
 
+-- include hidden files, except for .git
+M.telescope = {
+  n = {
+    ["<leader>ff"] = {"<cmd> Telescope find_files find_command=rg,--hidden,--files,--glob,!.git <CR>", "Find File"},
+  },
+}
+
 return M
