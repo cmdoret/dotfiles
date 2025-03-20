508c9cd128c9d03529aabdf013108f013d49f7bf
cmdoret <cyril.mattheydoret@gmail.com>
2025-03-20 15:49:20 +0100

    feat(nvim): b64 encode/decode

--- .config/nvchad/custom/mappings.lua
+++ .config/nvchad/custom/mappings.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+-- run system command base64 encode/decode
+M.general = {
+ v = {
+  ["<leader>be"] = {"c<c-r>=system('base64 --wrap=0', @\")<cr><esc>", "Base64 Encode"},
+  ["<leader>bd"] = {"c<c-r>=system('base64 --wrap=0 -d', @\")<cr><esc>", "Base64 Decode"},
+  }
+}
+
 M.maximizer = {
   n = {
     ["<leader>mm"] = {"<cmd> MaximizerToggle <CR>", "Maximize"},
